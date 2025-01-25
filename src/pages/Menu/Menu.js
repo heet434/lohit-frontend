d5b6cff5bd6219b31afa13d2893feb97908c4c6b
Heet Patel <hiheet04@gmail.com>
2025-01-25 15:46:31 +0530

    menu filters added

--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-scroll';
+import { useState, useEffect } from 'react';
 
 
 import { removeWhiteSpace } from '../../utils/strUtils';
@@ -7,8 +8,71 @@ import { removeWhiteSpace } from '../../utils/strUtils';
 import './Menu.css'
 import FoodCategory from '../../components/FoodCategory/FoodCategory';
 import Bestsellers from '../../components/Bestsellers/Bestsellers';
+import { use } from 'react';
 
 function Menu(props) {
+
+    //console.log(props)
+
+    const [itemsOnDisplay, setItemsOnDisplay] = useState([])
+    const [isVegDisplayed, setIsVegDisplayed] = useState(true)
+    const [isEggDisplayed, setIsEggDisplayed] = useState(true)
+    const [isNonVegDisplayed, setIsNonVegDisplayed] = useState(true)
+
+    useEffect(() => {
+        setItemsOnDisplay(props.menu)
+    }   ,  [props.menu])
+
+    const [vegIcon, setVegIcon] = useState(
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="17" viewBox="0 0 16 17" fill="none">
+            <rect y="0.5" width="16" height="16" rx="2" fill="#00BA34"/>
+            <path d="M5.86339 11.0826L3.08339 8.30263L2.13672 9.24263L5.86339 12.9693L13.8634 4.9693L12.9234 4.0293L5.86339 11.0826Z" fill="#F3F3E7"/>
+        </svg>)
+    const [eggIcon, setEggIcon] = useState(
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="17" viewBox="0 0 16 17" fill="none">
+            <rect y="0.5" width="16" height="16" rx="2" fill="#FFD700"/>
+            <path d="M5.86339 11.0826L3.08339 8.30263L2.13672 9.24263L5.86339 12.9693L13.8634 4.9693L12.9234 4.0293L5.86339 11.0826Z" fill="#F3F3E7"/>
+        </svg>)
+    const [nonVegIcon, setNonVegIcon] = useState(
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="17" viewBox="0 0 16 17" fill="none">
+            <rect y="0.5" width="16" height="16" rx="2" fill="#FF0000"/>
+            <path d="M5.86339 11.0826L3.08339 8.30263L2.13672 9.24263L5.86339 12.9693L13.8634 4.9693L12.9234 4.0293L5.86339 11.0826Z" fill="#F3F3E7"/>
+        </svg>)
+
+    const addItemsOfType = (type) => {
+        let fillColor = '#00BA34'
+        type === 'veg' ? fillColor='#00BA34' : type === 'egg' ? fillColor= '#FFD700' : fillColor = '#FF0000'
+
+        const filledSvg = 
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="17" viewBox="0 0 16 17" fill="none">
+            <rect y="0.5" width="16" height="16" rx="2" fill={fillColor}/>
+            <path d="M5.86339 11.0826L3.08339 8.30263L2.13672 9.24263L5.86339 12.9693L13.8634 4.9693L12.9234 4.0293L5.86339 11.0826Z" fill="#F3F3E7"/>
+        </svg>
+        type === 'veg' ? setVegIcon(filledSvg) : type === 'egg' ? setEggIcon(filledSvg) : setNonVegIcon(filledSvg)
+
+        let itemsToBeAdded = props.menu.filter((item) => {
+            return item.veg_nonveg_egg === type
+        })
+        console.log(itemsToBeAdded)
+        setItemsOnDisplay([...itemsOnDisplay, ...itemsToBeAdded])
+    }
+    const removeItemsOfType = (type) => {
+        let borderColour = '#00BA34'
+        type === 'veg' ? borderColour='#00BA34' : type === 'egg' ? borderColour= '#FFD700' : borderColour = '#FF0000'
+
+        const emptySvg = 
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+            <rect x="0.75" y="0.75" width="16.5" height="16.5" rx="1.25" fill="#F3F3E7" stroke={borderColour} strokeWidth="1.5" strokeLinejoin="round"/>
+        </svg>
+        type === 'veg' ? setVegIcon(emptySvg) : type === 'egg' ? setEggIcon(emptySvg) : setNonVegIcon(emptySvg)
+        
+        let itemsToBeRemoved = itemsOnDisplay.filter((item) => {
+            return item.veg_nonveg_egg === type
+        })
+        setItemsOnDisplay(itemsOnDisplay.filter((item) => {
+            return !itemsToBeRemoved.includes(item)
+        }))
+    }
 
     // const [foodCategories, setFoodCategories] = useState([])
     // const [menuItems, setMenuItems] = useState([])
@@ -114,31 +178,31 @@ function Menu(props) {
                     Menu
                 </div>
                 <div className='veg-nonveg-container'>
-                    <div className='veg-indicator menu-page-description-text'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="17" viewBox="0 0 16 17" fill="none">
-                            <rect y="0.5" width="16" height="16" rx="2" fill="#00BA34"/>
-                            <path d="M5.86339 11.0826L3.08339 8.30263L2.13672 9.24263L5.86339 12.9693L13.8634 4.9693L12.9234 4.0293L5.86339 11.0826Z" fill="#F3F3E7"/>
-                        </svg>
+                    <div className='veg-indicator menu-page-description-text' onClick={() => {
+                        isVegDisplayed ? removeItemsOfType('veg') : addItemsOfType('veg')
+                        setIsVegDisplayed(!isVegDisplayed)
+                    }}>
+                        {vegIcon}
                         veg only
                     </div>
-                    <div className='egg-indicator menu-page-description-text'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="17" viewBox="0 0 16 17" fill="none">
-                            <rect y="0.5" width="16" height="16" rx="2" fill="#FFD700"/>
-                            <path d="M5.86339 11.0826L3.08339 8.30263L2.13672 9.24263L5.86339 12.9693L13.8634 4.9693L12.9234 4.0293L5.86339 11.0826Z" fill="#F3F3E7"/>
-                        </svg>
+                    <div className='egg-indicator menu-page-description-text' onClick={() => {
+                        isEggDisplayed ? removeItemsOfType('egg') : addItemsOfType('egg')
+                        setIsEggDisplayed(!isEggDisplayed)
+                    }}>
+                        {eggIcon}
                         contains egg
                     </div>
-                    <div className='nonveg-indicator menu-page-description-text'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="17" viewBox="0 0 16 17" fill="none">
-                            <rect y="0.5" width="16" height="16" rx="2" fill="#FF0000"/>
-                            <path d="M5.86339 11.0826L3.08339 8.30263L2.13672 9.24263L5.86339 12.9693L13.8634 4.9693L12.9234 4.0293L5.86339 11.0826Z" fill="#F3F3E7"/>
-                        </svg>
+                    <div className='nonveg-indicator menu-page-description-text' onClick={() => {
+                        isNonVegDisplayed ? removeItemsOfType('non_veg') : addItemsOfType('non_veg')
+                        setIsNonVegDisplayed(!isNonVegDisplayed)
+                    }}>
+                        {nonVegIcon}
                         non-veg only
                     </div>
                 </div>
                 {foodCategories.map((item) => {
                     return(
-                        <FoodCategory key={item.id} categoryName={item.name} idName={removeWhiteSpace(item.name)} menuItems = {props.menu}/>
+                        <FoodCategory key={item.id} categoryName={item.name} idName={removeWhiteSpace(item.name)} menuItems = {itemsOnDisplay} />
                     )
                 })}
                 <Bestsellers categoryName="Bestsellers" bestsellers = {bestsellers}/>
